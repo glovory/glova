6e3223c2601543e2d722dfea8dc5c119d69d31f2
romaiswara <rhomajr27@gmail.com>
2020-03-26 11:58:54 +0700

    🥣 config color

---
+++ lib/src/model/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class ColorConfig {
+  static const Color black = Color(0xFF000000);
+  static const Color transparent = Color(0x00000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color primary = Color(0xFF3467fc);
+  static const Color success = Color(0xFF6de196);
+  static const Color info = Color(0xFF1294fc);
+  static const Color warning = Color(0xFFf6a829);
+  static const Color danger = Color(0xFFed576f);
+}
