dca88df2a3021ccc63940bd32b3d7093a66bccd9
romaiswara <rhomajr27@gmail.com>
2020-03-26 14:20:15 +0700

    🔄 refactor colorConfig to TuxColor n set color based on design

--- lib/src/model/colors.dart
+++ lib/src/model/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/widgets.dart';
 
-class ColorConfig {
+class TuxColor {
   static const Color black = Color(0xFF000000);
   static const Color transparent = Color(0x00000000);
   static const Color white = Color(0xFFFFFFFF);
 
-  static const Color primary = Color(0xFF3467fc);
+  static const Color primary = Color(0xFF0049E2);
   static const Color success = Color(0xFF6de196);
   static const Color info = Color(0xFF1294fc);
   static const Color warning = Color(0xFFf6a829);
-  static const Color danger = Color(0xFFed576f);
+  static const Color danger = Color(0xFFFA4938);
+  static const Color background_default = Color(0xFFF7F9FC);
 }
