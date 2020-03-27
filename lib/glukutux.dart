643e5f8558ac61350b389c79733ec3146a13ec9c
romaiswara <rhomajr27@gmail.com>
2020-03-27 22:18:00 +0700

    🥕 add radio glukutux

--- lib/glukutux.dart
+++ lib/glukutux.dart
@@ -5,6 +5,7 @@ export 'package:glukutux/src/model/params.dart';
 // Components
 export 'package:glukutux/src/components/button/button.dart';
 export 'package:glukutux/src/components/check_box/check_box.dart';
+export 'package:glukutux/src/components/radio_button/radio_button.dart';
 export 'package:glukutux/src/components/text_form_field/text_form_field.dart';
 export 'package:glukutux/src/components/toogle/toogle.dart';
 
---
+++ lib/src/components/radio_button/radio_button.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:glukutux/glukutux.dart';
+
+class TuxRadioButton extends StatefulWidget {
+  final TuxStatus tuxStatus;
+  final List radioValues;
+  final List<String> radioLables;
+  final ValueChanged<dynamic> onChanged;
+  final bool horizontal;
+  final bool enabled;
+
+  const TuxRadioButton({
+    this.tuxStatus = TuxStatus.primary,
+    @required this.radioLables,
+    @required this.radioValues,
+    this.onChanged,
+    this.horizontal = false,
+    this.enabled = true,
+  });
+
+  _TuxRadioButtonState createState() => _TuxRadioButtonState();
+}
+
+class _TuxRadioButtonState extends State<TuxRadioButton> {
+  dynamic currentSelectedValue = 0;
+  String currentSelectedLabel;
+
+  @override
+  void initState() {
+    super.initState();
+    currentSelectedLabel = widget.radioLables[0];
+  }
+
+  void changeValue(dynamic value, int index) {
+    if (!widget.enabled) {
+      return;
+    }
+    widget.onChanged(widget.radioValues[index]);
+    setState(() {
+      currentSelectedValue = widget.radioValues[index];
+    });
+  }
+
+  Color tuxColor({TuxStatus status}) {
+    switch (status) {
+      case TuxStatus.primary:
+        return TuxColor.primary;
+      case TuxStatus.success:
+        return TuxColor.success;
+      case TuxStatus.info:
+        return TuxColor.info;
+      case TuxStatus.warning:
+        return TuxColor.warning;
+      case TuxStatus.danger:
+        return TuxColor.danger;
+      default:
+        return TuxColor.primary;
+    }
+  }
+
+  Color colorRadio() {
+    if (widget.enabled) {
+      return tuxColor(status: widget.tuxStatus);
+    } else {
+      return Colors.grey;
+    }
+  }
+
+  List<Widget> buildRadioRow() {
+    List<Widget> radio = [];
+    for (int index = 0; index < widget.radioLables.length; index++) {
+      radio.add(
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Radio(
+              value: widget.radioValues[index],
+              groupValue: currentSelectedValue,
+              activeColor: colorRadio(),
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              onChanged: (value) {
+                changeValue(value, index);
+              },
+            ),
+            GestureDetector(
+              onTap: () {
+                changeValue(widget.radioValues[index], index);
+              },
+              child: Text(widget.radioLables[index]),
+            )
+          ],
+        ),
+      );
+    }
+    return radio;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(unselectedWidgetColor: colorRadio()),
+      child: (widget.horizontal)
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: buildRadioRow(),
+            )
+          : IntrinsicWidth(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: buildRadioRow(),
+              ),
+            ),
+    );
+  }
+}
