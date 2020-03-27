41cc99217f5fa0b9f6081cf5d29ddfd59b5a6cb7
romaiswara <rhomajr27@gmail.com>
2020-03-27 12:13:34 +0700

    🍺 add toogle glukutux

--- lib/glukutux.dart
+++ lib/glukutux.dart
@@ -5,6 +5,7 @@ export 'package:glukutux/src/model/params.dart';
 // Components
 export 'package:glukutux/src/components/button/button.dart';
 export 'package:glukutux/src/components/text_form_field/text_form_field.dart';
+export 'package:glukutux/src/components/toogle/toogle.dart';
 
 // Eva Icons
 export 'package:eva_icons_flutter/eva_icons_flutter.dart';
---
+++ lib/src/components/toogle/toogle.dart
@@ -0,0 +1,128 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+import '../../../glukutux.dart';
+
+class TuxToogle extends StatefulWidget {
+  final TuxStatus tuxStatus;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final bool enable;
+
+  const TuxToogle({
+    this.tuxStatus = TuxStatus.primary,
+    this.value,
+    this.onChanged,
+    this.enable = true,
+  });
+
+  @override
+  _TuxToogleState createState() => _TuxToogleState();
+}
+
+class _TuxToogleState extends State<TuxToogle>
+    with SingleTickerProviderStateMixin {
+  Animation _circleAnimation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 60));
+    _circleAnimation = AlignmentTween(
+            begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
+            end: widget.value ? Alignment.centerLeft : Alignment.centerRight)
+        .animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.linear));
+  }
+
+  Color tuxColor({TuxStatus status}) {
+    switch (status) {
+      case TuxStatus.primary:
+        return TuxColor.primary;
+      case TuxStatus.success:
+        return TuxColor.success;
+      case TuxStatus.info:
+        return TuxColor.info;
+      case TuxStatus.warning:
+        return TuxColor.warning;
+      case TuxStatus.danger:
+        return TuxColor.danger;
+      default:
+        return TuxColor.primary;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color colorToogle;
+    if (widget.enable) {
+      colorToogle = tuxColor(status: widget.tuxStatus);
+    } else {
+      colorToogle = Colors.grey;
+    }
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return GestureDetector(
+          onTap: () {
+            if (!widget.enable) {
+              return;
+            }
+            if (_animationController.isCompleted) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+            widget.value == false
+                ? widget.onChanged(true)
+                : widget.onChanged(false);
+          },
+          child: Container(
+            width: 65.0,
+            height: 40.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: _circleAnimation.value == Alignment.centerLeft
+                    ? colorToogle.withOpacity(0.3)
+                    : colorToogle),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _circleAnimation.value == Alignment.centerRight
+                    ? Padding(
+                        padding: const EdgeInsets.only(right: 25.0),
+                      )
+                    : Container(),
+                Align(
+                  alignment: _circleAnimation.value,
+                  child: Container(
+                    width: 30.0,
+                    height: 30.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: (_circleAnimation.value == Alignment.centerRight)
+                        ? Icon(
+                            EvaIcons.checkmark,
+                            size: 25,
+                            color: colorToogle,
+                          )
+                        : null,
+                  ),
+                ),
+                _circleAnimation.value == Alignment.centerLeft
+                    ? Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
+                      )
+                    : Container(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
