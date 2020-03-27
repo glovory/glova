d2b454a1d843f9c0c23d19e87134d48ce1aa849f
romaiswara <rhomajr27@gmail.com>
2020-03-27 10:02:33 +0700

    🍹 add tux text form field

--- lib/glukutux.dart
+++ lib/glukutux.dart
@@ -4,6 +4,7 @@ export 'package:glukutux/src/model/params.dart';
 
 // Components
 export 'package:glukutux/src/components/button/button.dart';
+export 'package:glukutux/src/components/text_form_field/text_form_field.dart';
 
 // Eva Icons
 export 'package:eva_icons_flutter/eva_icons_flutter.dart';
---
+++ lib/src/components/text_form_field/text_form_field.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:glukutux/glukutux.dart';
+
+class TuxTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final TextCapitalization textCapitalization;
+  final TextStyle style;
+  final bool obscureText;
+  final ValueChanged<String> onChanged;
+  final GestureTapCallback onTap;
+  final VoidCallback onEditingComplete;
+  final ValueChanged<String> onFieldSubmitted;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final bool enabled;
+  final EdgeInsetsGeometry contentPadding;
+  final TuxStatus tuxStatus;
+  final double widthBorder;
+  final double radius;
+  final String hint;
+  final Icon icon;
+  final bool filled;
+  final bool isPrefix;
+  final String label;
+  final String description;
+
+  const TuxTextFormField({
+    this.controller,
+    this.keyboardType,
+    this.textCapitalization = TextCapitalization.none,
+    this.style,
+    this.obscureText = false,
+    this.onChanged,
+    this.onTap,
+    this.onEditingComplete,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.validator,
+    this.enabled,
+    this.contentPadding,
+    this.tuxStatus,
+    this.widthBorder = 1,
+    this.radius = 4,
+    this.hint,
+    this.icon,
+    this.filled = false,
+    this.isPrefix = false,
+    this.label,
+    this.description,
+  });
+
+  const TuxTextFormField.withPrefixIcon({
+    this.controller,
+    this.keyboardType,
+    this.textCapitalization = TextCapitalization.none,
+    this.style,
+    this.obscureText = false,
+    this.onChanged,
+    this.onTap,
+    this.onEditingComplete,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.validator,
+    this.enabled,
+    this.contentPadding,
+    this.tuxStatus,
+    this.widthBorder = 1,
+    this.radius = 4,
+    this.hint,
+    this.icon,
+    this.filled = false,
+    this.label,
+    this.description,
+  }) : this.isPrefix = true;
+
+  const TuxTextFormField.withSuffixIcon({
+    this.controller,
+    this.keyboardType,
+    this.textCapitalization = TextCapitalization.none,
+    this.style,
+    this.obscureText = false,
+    this.onChanged,
+    this.onTap,
+    this.onEditingComplete,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.validator,
+    this.enabled,
+    this.contentPadding,
+    this.tuxStatus,
+    this.widthBorder = 1,
+    this.radius = 4,
+    this.hint,
+    this.icon,
+    this.filled = false,
+    this.label,
+    this.description,
+  }) : this.isPrefix = false;
+
+  Color tuxColor({TuxStatus status}) {
+    switch (status) {
+      case TuxStatus.primary:
+        return TuxColor.primary;
+      case TuxStatus.success:
+        return TuxColor.success;
+      case TuxStatus.info:
+        return TuxColor.info;
+      case TuxStatus.warning:
+        return TuxColor.warning;
+      case TuxStatus.danger:
+        return TuxColor.danger;
+      default:
+        return TuxColor.primary;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(primaryColor: tuxColor(status: tuxStatus)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          if (label != null) ...[
+            Text(
+              label.toUpperCase(),
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 6.0),
+          ],
+          TextFormField(
+            controller: controller,
+            keyboardType: keyboardType,
+            textCapitalization: textCapitalization,
+            style: style,
+            obscureText: obscureText,
+            onChanged: onChanged,
+            onTap: onTap,
+            onEditingComplete: onEditingComplete,
+            onFieldSubmitted: onFieldSubmitted,
+            onSaved: onSaved,
+            validator: validator,
+            enabled: enabled,
+            decoration: InputDecoration(
+              prefixIcon: (isPrefix) ? icon : null,
+              suffixIcon: (!isPrefix) ? icon : null,
+              filled: filled,
+              contentPadding: contentPadding,
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius),
+                borderSide: BorderSide(
+                  width: 0.2,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius),
+                borderSide: BorderSide(
+                  width: 0.2,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius),
+                borderSide: BorderSide(
+                  color: tuxColor(status: tuxStatus),
+                  width: widthBorder,
+                ),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius),
+                borderSide: BorderSide(
+                  color: TuxColor.danger,
+                  width: widthBorder,
+                ),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius),
+                borderSide: BorderSide(
+                  color: TuxColor.danger,
+                  width: widthBorder,
+                ),
+              ),
+              errorStyle: TextStyle(
+                color: TuxColor.danger,
+              ),
+              hintText: hint,
+            ),
+          ),
+          if (description != null) ...[
+            SizedBox(height: 6.0),
+            Text(
+              description,
+              style: TextStyle(),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
