2ece62407c037cbb489f55e7650bc96adb9194e8
enggarsaka95 <enggarsaka95@gmail.com>
2020-05-04 12:06:24 +0700

    🎇 [ADD] export icon_button

--- lib/glukutux.dart
+++ lib/glukutux.dart
@@ -22,6 +22,7 @@ export 'package:glukutux/src/component/tabs/tab_bar.dart';
 export 'package:glukutux/src/component/tabs/tab.dart';
 export 'package:glukutux/src/component/text_form_field/text_form_field.dart';
 export 'package:glukutux/src/component/toogle/toogle.dart';
+export 'package:glukutux/src/component/icon_botton/icon_button.dart';
 
 // Eva Icons
 export 'package:eva_icons_flutter/eva_icons_flutter.dart';
