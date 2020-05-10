6ac529ee67a71522ae37e660aacf9cf3940f1ff0
enggarsaka95 <enggarsaka95@gmail.com>
2020-05-10 18:22:05 +0700

    🎆 Example of generating json files into darts

---
+++ lib/color/tux_color.dart
@@ -0,0 +1,112 @@
+import 'dart:ui';
+import 'package:glukutux/glukutux.dart';
+
+
+//@autogenerate do not edit
+class TuxColorScheme {
+   static const TuxColor primary = TuxColor(0xFF3366FF, {
+      100 :  Color(0xFFD6E4FF),
+      200 :  Color(0xFFADC8FF),
+      300 :  Color(0xFF84A9FF),
+      400 :  Color(0xFF6690FF),
+      500 :  Color(0xFF3366FF),
+      600 :  Color(0xFF254EDB),
+      700 :  Color(0xFF1939B7),
+      800 :  Color(0xFF102693),
+      900 :  Color(0xFF091A7A),
+      
+   });
+   static const TuxColor success = TuxColor(0xFF30C148, {
+      100 :  Color(0xFFDEFBD6),
+      200 :  Color(0xFFB6F8AE),
+      300 :  Color(0xFF84EC81),
+      400 :  Color(0xFF5FD967),
+      500 :  Color(0xFF30C148),
+      600 :  Color(0xFF23A545),
+      700 :  Color(0xFF188A40),
+      800 :  Color(0xFF0F6F3A),
+      900 :  Color(0xFF095C36),
+      
+   });
+   static const TuxColor info = TuxColor(0xFF4E93FC, {
+      100 :  Color(0xFFDBEFFE),
+      200 :  Color(0xFFB8DCFE),
+      300 :  Color(0xFF94C7FE),
+      400 :  Color(0xFF7AB3FD),
+      500 :  Color(0xFF4E93FC),
+      600 :  Color(0xFF3971D8),
+      700 :  Color(0xFF2754B5),
+      800 :  Color(0xFF183A92),
+      900 :  Color(0xFF0E2778),
+      
+   });
+   static const TuxColor warning = TuxColor(0xFFFFBE3D, {
+      100 :  Color(0xFFFFF7D8),
+      200 :  Color(0xFFFFEDB1),
+      300 :  Color(0xFFFFE08A),
+      400 :  Color(0xFFFFD36D),
+      500 :  Color(0xFFFFBE3D),
+      600 :  Color(0xFFDB9A2C),
+      700 :  Color(0xFFB7791E),
+      800 :  Color(0xFF935B13),
+      900 :  Color(0xFF7A460B),
+      
+   });
+   static const TuxColor danger = TuxColor(0xFFFF463D, {
+      100 :  Color(0xFFFFE7D8),
+      200 :  Color(0xFFFFC8B1),
+      300 :  Color(0xFFFFA38A),
+      400 :  Color(0xFFFF806D),
+      500 :  Color(0xFFFF463D),
+      600 :  Color(0xFFDB2C33),
+      700 :  Color(0xFFB71E31),
+      800 :  Color(0xFF93132D),
+      900 :  Color(0xFF7A0B2B),
+      
+   });
+   static const TuxColor primaryTransparent = TuxColor(0x66166ff, {
+      100 :  Color(0x14166ff),
+      200 :  Color(0x28166ff),
+      300 :  Color(0x3d166ff),
+      400 :  Color(0x51166ff),
+      500 :  Color(0x66166ff),
+      600 :  Color(0x7a166ff),
+      
+   });
+   static const TuxColor successTransparent = TuxColor(0x668c148, {
+      100 :  Color(0x148c148),
+      200 :  Color(0x288c148),
+      300 :  Color(0x3d8c148),
+      400 :  Color(0x518c148),
+      500 :  Color(0x668c148),
+      600 :  Color(0x7a8c148),
+      
+   });
+   static const TuxColor infoTransparent = TuxColor(0x66893fc, {
+      100 :  Color(0x14893fc),
+      200 :  Color(0x28893fc),
+      300 :  Color(0x3d893fc),
+      400 :  Color(0x51893fc),
+      500 :  Color(0x66893fc),
+      600 :  Color(0x7a893fc),
+      
+   });
+   static const TuxColor warningTransparent = TuxColor(0x6637be3d, {
+      100 :  Color(0x1437be3d),
+      200 :  Color(0x2837be3d),
+      300 :  Color(0x3d37be3d),
+      400 :  Color(0x5137be3d),
+      500 :  Color(0x6637be3d),
+      600 :  Color(0x7a37be3d),
+      
+   });
+   static const TuxColor dangerTransparent = TuxColor(0x6637463d, {
+      100 :  Color(0x1437463d),
+      200 :  Color(0x2837463d),
+      300 :  Color(0x3d37463d),
+      400 :  Color(0x5137463d),
+      500 :  Color(0x6637463d),
+      600 :  Color(0x7a37463d),
+      
+   });
+}
