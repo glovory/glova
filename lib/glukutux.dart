fac4e61d66eb95df80359611f3d19f9cbf3a8c2e
romaiswara <iswararoma@gmail.com>
2020-05-13 17:58:31 +0700

    🧢 Add size, padding, label, position. Refactor toogle;

--- lib/glukutux.dart
+++ lib/glukutux.dart
@@ -21,7 +21,7 @@ export 'package:glukutux/src/component/spinner/spinner.dart';
 export 'package:glukutux/src/component/tabs/tab_bar.dart';
 export 'package:glukutux/src/component/tabs/tab.dart';
 export 'package:glukutux/src/component/text_form_field/text_form_field.dart';
-export 'package:glukutux/src/component/toogle/toogle.dart';
+export 'package:glukutux/src/component/toggle/toggle.dart';
 
 // Eva Icons
 export 'package:eva_icons_flutter/eva_icons_flutter.dart';
