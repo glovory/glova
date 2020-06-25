3a81923b0ea7a644d57db21000ae31d4c4dc0c67
romaiswara <iswararoma@gmail.com>
2020-06-25 11:28:39 +0700

    🎆 [ADD] text theme

--- lib/src/core/theme_data.dart
+++ lib/src/core/theme_data.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 
 import '../component/button_theme.dart';
+import '../component/text_theme.dart';
 import 'color_swatch.dart';
 
 /// Theme data for OvaApp.
@@ -12,20 +13,26 @@ class OvaThemeData with Diagnosticable {
   /// Button theme data, with fallback to default
   final OvaButtonThemeData buttonTheme;
 
+  /// Text theme
+  final OvaTextTheme textTheme;
+
   /// Factory constructor that has fallback for the undefined parameters.
   factory OvaThemeData({
     OvaColorSwatch colorSwatch,
     OvaButtonThemeData buttonTheme,
+    OvaTextTheme textTheme,
   }) {
     colorSwatch ??= OvaColorSwatch(); // set default color swatch
     buttonTheme = OvaButtonThemeData.merge(
       theme: buttonTheme ??= OvaButtonThemeData.fallback(colorSwatch),
       swatch: colorSwatch,
     );
+    textTheme = OvaTextTheme.merge(textTheme: textTheme ??= OvaTextTheme.defaultTextTheme());
 
     return OvaThemeData.raw(
       colorSwatch: colorSwatch,
       buttonTheme: buttonTheme,
+      textTheme: textTheme,
     );
   }
 
@@ -33,5 +40,6 @@ class OvaThemeData with Diagnosticable {
   OvaThemeData.raw({
     @required this.colorSwatch,
     this.buttonTheme,
+    this.textTheme,
   }) : assert(colorSwatch != null);
 }
