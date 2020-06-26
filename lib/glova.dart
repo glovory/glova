972ef66926f600c164424bf29b806aab490a4dff
romaiswara <iswararoma@gmail.com>
2020-06-26 10:52:11 +0700

    🎍 [ADD] export spinner

--- lib/glova.dart
+++ lib/glova.dart
@@ -4,6 +4,7 @@ export 'package:eva_color/eva_color.dart';
 
 export 'src/component/app.dart';
 export 'src/component/button.dart';
+export 'src/component/spinner.dart';
 export 'src/component/button_theme.dart';
 export 'src/component/text_theme.dart';
 export 'src/core/color_swatch.dart';
